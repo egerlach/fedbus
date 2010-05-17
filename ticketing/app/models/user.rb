f97c4057be62383304770748ac534be5b920baa3
Eric Gerlach <egerlach@feds.uwaterloo.ca>
2010-05-17 18:38:28 -0400

    Add validation to the email address for the user
    Regexp came from here: http://www.regular-expressions.info/email.html
    
    Used the slightly easier version of the correct one.  Note that the {}
    and $ had to be escaped.
    
    refs #8

--- ticketing/app/models/user.rb
+++ ticketing/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+  validates_format_of :email, :with => /[a-z0-9!#\$%&'*+\/=?^_`\{|\}~-]+(?:\.[a-z0-9!#\$%&'*+\/=?^_`\{|\}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+
+  def student_number=(n)
+    self.student_number_hash = Digest::SHA256.hexdigest(n)
+  end
 end
--- ticketing/test/unit/user_test.rb
+++ ticketing/test/unit/user_test.rb
@@ -2,7 +2,21 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Setting User student number stores the hash" do
+    u = User.new
+    u.student_number = '00000000'
+    assert(u.student_number_hash == '7e071fd9b023ed8f18458a73613a0834f6220bd5cc50357ba3493c6040a9ea8c')
+  end
+
+  test "Valid email address accepted" do
+    u = User.new
+    u.email = 'testing@uwaterloo.ca'
+    assert(u.save, "User rejects a valid email address")
+  end
+
+  test "Invalid email address rejected" do
+    u = User.new
+    u.email = 'notavalidemail@something'
+    assert(!u.save, "User accepts an invalid email address")
   end
 end
