fedbd171817c206981613ff1c7b236dcbfccced5
Eric Gerlach <egerlach@feds.uwaterloo.ca>
2010-05-18 15:31:58 -0400

    Add validation for User attributes
    A user must have a first_name, last_name, and email.  Student_number is
    optional, but must be confirmed.  Also edited the new/edit views to
    match.
    
    Changing of the fixtures broke all the functional tests, though.
    
    refs #8

--- ticketing/app/models/user.rb
+++ ticketing/app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ActiveRecord::Base
 
   validates_format_of :email, :with => /[a-z0-9!#\$%&'*+\/=?^_`\{|\}~-]+(?:\.[a-z0-9!#\$%&'*+\/=?^_`\{|\}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  validates_presence_of :first_name, :last_name
+  validates_presence_of :student_number, :student_number_confirmation, :if => :student_number_changed?
+  validates_confirmation_of :student_number
+
+  attr_accessor :student_number, :student_number_confirmation
 
   def student_number=(n)
-    self.student_number_hash = Digest::SHA256.hexdigest(n)
+    @student_number=n
+    @student_number_hash = Digest::SHA256.hexdigest(n)
   end
+
+  def student_number_changed?
+    @student_number || @student_number_confirmation
+  end
+
 end
--- ticketing/test/unit/user_test.rb
+++ ticketing/test/unit/user_test.rb
@@ -1,22 +1,58 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+
+  fixtures :users
+
   test "Setting User student number stores the hash" do
-    u = User.new
+    u = users(:tester)
     u.student_number = '00000000'
-    assert(u.student_number_hash == '7e071fd9b023ed8f18458a73613a0834f6220bd5cc50357ba3493c6040a9ea8c')
+    assert u.student_number_hash == '7e071fd9b023ed8f18458a73613a0834f6220bd5cc50357ba3493c6040a9ea8c'
   end
 
   test "Valid email address accepted" do
-    u = User.new
+    u = users(:tester)
     u.email = 'testing@uwaterloo.ca'
-    assert(u.save, "User rejects a valid email address")
+    assert u.save, "User rejects a valid email address: " + u.errors.full_messages.inspect
   end
 
   test "Invalid email address rejected" do
-    u = User.new
+    u = users(:tester)
     u.email = 'notavalidemail@something'
-    assert(!u.save, "User accepts an invalid email address")
+    assert !u.save, "User accepts an invalid email address: " + u.errors.full_messages.inspect
+  end
+
+  test "User requires first name" do
+    u = users(:tester)
+    u.first_name = nil
+    assert !u.save, "User accepts an entry without first name: " + u.errors.full_messages.inspect
+  end
+
+  test "User requires last name" do
+    u = users(:tester)
+    u.last_name = nil
+    assert !u.save, "User accepts an entry without last name: " + u.errors.full_messages.inspect
+  end
+
+  test "User requires email" do
+    u = users(:tester)
+    u.email = nil
+    assert !u.save, "User accepts an entry without email: " + u.errors.full_messages.inspect
+  end
+
+  test "Setting student_number requires confirmation" do
+    u = users(:tester)
+    u.student_number = '00000000'
+    assert !u.save, "User.student_number can be set without confirmation: " + u.errors.full_messages.inspect
+
+    u = users(:tester)
+    u.student_number = '00000000'
+    u.student_number_confirmation = '11111111'
+    assert !u.save, "User.student_number can be set with invalid confirmation: " + u.errors.full_messages.inspect
+
+    u = users(:tester)
+    u.student_number = '12345678'
+    u.student_number_confirmation = '12345678'
+    assert u.save, "User.student_number cannot be set with valid confirmation: " + u.errors.full_messages.inspect
   end
 end
