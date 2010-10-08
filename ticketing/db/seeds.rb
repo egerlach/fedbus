affc08a417f87f094152c9ae02280617374d4d61
Brad Krane <bjkrane@feds.uwaterloo.ca>
2010-10-08 16:58:24 -0400

    Put permission seeds into the database seeds file for blackouts, holidays, trips and buses. closes #33
    Signed-off-by: Eric Gerlach <egerlach@feds.uwaterloo.ca>

--- ticketing/db/seeds.rb
+++ ticketing/db/seeds.rb
@@ -1,6 +1,10 @@
 # Permission to administer the application's access control components,
 # including users and authorization.
 Permission.find_or_create_by_name("Manage access control")
+Permission.find_or_create_by_name("Trips")
+Permission.find_or_create_by_name("Buses")
+Permission.find_or_create_by_name("Blackouts")
+Permission.find_or_create_by_name("Holidays")
 
 # Default roles
 admin = Role.find_or_create_by_name("Administrator")
