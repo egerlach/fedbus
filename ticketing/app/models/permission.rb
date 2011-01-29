52ba04c946f6cfc89c345cd24f58600a4336bd0c
Eric Gerlach <egerlach@feds.uwaterloo.ca>
2011-01-29 17:56:41 -0500

    Auto-capitalises permission names
    This way they always correspond with their symbol versions
    
    closes #56
    
    Coded by: Julien Bergeron

--- ticketing/app/models/permission.rb
+++ ticketing/app/models/permission.rb
@@ -2,4 +2,10 @@ class Permission < ActiveRecord::Base
   has_and_belongs_to_many :roles
   validates_presence_of :name
   validates_uniqueness_of :name
+  
+  #Properly capitalizes the permission name because otherwise we'll get problems elsewhere.
+  def name=(new_name)
+    self.write_attribute(:name, new_name.capitalize)
+  end
+  
 end
