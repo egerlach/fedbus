8ab0f0742dfb484d5b25d7334852c0b5df6eebbb
Brad Krane <bjkrane@feds.uwaterloo.ca>
2011-01-28 20:08:42 -0800

    Added functional tests to the buses controller.
    refs #31
    
    Signed-off-by: Eric Gerlach <egerlach@feds.uwaterloo.ca>

--- ticketing/test/functional/buses_controller_test.rb
+++ ticketing/test/functional/buses_controller_test.rb
@@ -7,12 +7,39 @@ class BusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:buses)
   end
 
-  test "should get new" do
+  test "should not get new for unauthenticated user and redirect to login" do
+    get :new
+    assert_response :redirect
+  end
+
+  test "should not get new for authenticated user without busses permission" do
+    @request.session[:userid] = users(:one).userid
+    get :new
+    assert_response :forbidden
+  end
+
+  test "should get new for user with buses permission" do
+    setup_authenticated_user_with_permission :tester, :buses
     get :new
     assert_response :success
   end
 
-  test "should create bus" do
+  test "should not create bus for unauthenticated user and redirect to login" do
+    assert_difference('Bus.count', 0) do
+      post :create, :bus => buses(:one)
+    end
+    assert_response :redirect
+  end
+
+  test "should not create new for authenticated user without buses permission" do
+    assert_difference('Bus.count', 0) do
+      post :create, :bus => buses(:one)
+    end
+    assert_response :redirect
+  end
+
+  test "should create bus for user with buses permission" do
+    setup_authenticated_user_with_permission :tester, :buses
     assert_difference('Bus.count') do
       post :create, :bus => { 
         :name => 'Something',
@@ -28,26 +55,76 @@ class BusesControllerTest < ActionController::TestCase
     assert_redirected_to bus_path(assigns(:bus))
   end
 
-  test "should show bus" do
+  test "should show bus for an unauthenticated user" do
     get :show, :id => buses(:one).to_param
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should show bus for authenticated user without buses permission" do
+    @request.session[:userid] = users(:one).userid
+    get :show, :id => buses(:one).to_param
+    assert_response :success
+  end
+
+  test "should show bus for authenticated user with buses permission" do
+    setup_authenticated_user_with_permission :tester, :buses
+    get :show, :id => buses(:one).to_param
+    assert_response :success
+  end
+
+  test "should not get edit for unauthenticated user and redirect to login" do
+    get :edit, :id => buses(:one).to_param
+    assert_response :redirect
+  end
+    
+  test "should not get edit for authenticated user without buses permission" do
+    @request.session[:userid] = users(:one).userid
+    get :edit, :id => buses(:one).to_param
+    assert_response :forbidden
+  end
+
+  test "should get edit for user with buses permission" do
+    setup_authenticated_user_with_permission :tester, :buses
     get :edit, :id => buses(:one).to_param
     assert_response :success
   end
 
-  test "should update bus" do
+  test "should not update bus for unauthenticated user and redirect to login" do
+    put :update, :id => buses(:one).to_param, :bus => { :status => :locked }
+    assert_response :redirect
+  end
+
+  test "should not update bus for authenticated user without buses permission" do
+    @request.session[:userid] = users(:one).userid
+    put :update, :id => buses(:one).to_param, :bus => { :status => :locked }
+    assert_response :forbidden
+  end
+
+  test "should update bus for user with buses permission" do
+    setup_authenticated_user_with_permission :tester, :buses
     put :update, :id => buses(:one).to_param, :bus => { :status => :locked }
     assert_redirected_to bus_path(assigns(:bus))
   end
 
-  test "should destroy bus" do
+  test "should not delete bus for unauthenticated user and redirect to login" do
+    assert_difference('Bus.count', 0) do
+      delete :destroy, :id => buses(:one).to_param
+    end
+    assert_response :redirect
+  end
+
+  test "should not delete bus for authenticated user without buses permission" do
+    assert_difference('Bus.count', 0) do
+      delete :destroy, :id => buses(:one).to_param
+    end
+    assert_response :redirect
+  end
+
+  test "should destroy bus for authenticated user with buses permission" do
+    setup_authenticated_user_with_permission :tester, :buses
     assert_difference('Bus.count', -1) do
       delete :destroy, :id => buses(:one).to_param
     end
-
     assert_redirected_to buses_path
   end
 end
