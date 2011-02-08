4a8c2f2633e7ac6f421eeb33185718397084fff1
Daniel Manary <dmanary@uwaterloo.ca>
2011-02-08 16:57:01 -0500

    Updated the application controller

--- ticketing/app/controllers/application_controller.rb
+++ ticketing/app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+require 'authorization.rb'
+
 class ApplicationController < ActionController::Base
   include Authorization::ControllerMixin
-  protect_from_forgery
   helper :all # include all helpers, all the time
   helper_method :current_user, :logged_in?
 
